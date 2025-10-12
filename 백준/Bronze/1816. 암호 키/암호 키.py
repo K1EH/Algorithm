f79a4544d3072b877460b46335e9a37dd1e836c3
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2025-10-12 16:12:08 +0900

    [Bronze I] Title: 암호 키, Time: 1260 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1816. 암호 키/암호 키.py
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -1,9 +1,14 @@
-tc = int(input())
-for _ in range(tc):
+PRIME_LIMIT = 1_000_000
+
+N = int(input())
+for _ in range(N):
     num = int(input())
-    for i in range(2, 1_000_001):
-        if num % i == 0:
+    if num <= PRIME_LIMIT:
+        print("NO")
+        continue
+    for p in range(2, PRIME_LIMIT + 1):
+        if num % p == 0:
             print("NO")
             break
-        if i == 1_000_000:
-            print("YES")
+    else:
+        print("YES")

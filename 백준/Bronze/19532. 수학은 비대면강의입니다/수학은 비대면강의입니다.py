72d26846e21dec0c7e34a1bef8421b0f56272652
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2025-10-12 19:39:22 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,13 +1,6 @@
 a, b, c, d, e, f = map(int, input().split())
-for x in range(-999, 1000):
-    if b != 0:
-        if (c - a * x) / b == (c - a * x) // b:
-            y = (c - a * x) // b
-            if d * x + e * y == f:
-                print(x, y)
-                break
-    else:
-        x = c // a
-        y = (f - d * x) // e
-        print(x, y)
-        break
+det = a * e - b * d
+detv1 = c * e - b * f
+detv2 = a * f - c * d
+
+print(detv1 // det, detv2 // det)

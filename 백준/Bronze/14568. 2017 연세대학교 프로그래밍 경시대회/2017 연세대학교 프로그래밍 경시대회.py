916240a8e4f71602c0de492524f44ac8c731fa3d
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2025-10-12 19:36:29 +0900

    [Bronze III] Title: 2017 연세대학교 프로그래밍 경시대회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
+++ 백준/Bronze/14568. 2017 연세대학교 프로그래밍 경시대회/2017 연세대학교 프로그래밍 경시대회.py
@@ -1,11 +1,8 @@
-N = int(input())
-
-count = 0
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        for k in range(1, N + 1):
-            if i + j + k == N:
-                if k % 2 == 0:
-                    if i >= j + 2:
-                        count += 1
-print(count)
+candy = int(input())
+ans = 0
+# an even number of candies
+for c in range(2, candy + 1, 2):
+    for b in range(1, (candy - c) + 1):
+        if candy - b - c >= b + 2:
+            ans += 1
+print(ans)

10206a83a606f33a592212d9d4707294f9a35f22
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2025-10-18 01:25:21 +0900

    [Gold V] Title: 토마토, Time: 3656 ms, Memory: 50300 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -1,51 +1,30 @@
 from collections import deque
 
-X, Y, H = map(int, input().split())
-graph = []
-for h in range(H):
-    xy = []
-    for y in range(Y):
-        xy.append(list(map(int, input().split())))
-    graph.append(xy)
+M, N, H = map(int, input().split())
+graph = [[list(map(int, input().split())) for _ in range(N)] for _ in range(H)]
 
 directions = ((1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1))
-visited = [[[False] * X for _ in range(Y)] for _ in range(H)]
-already_riped = True
-cannot_riped = False
-ans = 0
-
 queue = deque()
+
 for h in range(H):
-    for y in range(Y):
-        for x in range(X):
-            if graph[h][y][x] == 0:
-                already_riped = False
+    for y in range(N):
+        for x in range(M):
             if graph[h][y][x] == 1:
                 queue.append((h, y, x))
-                visited[h][y][x] = True
+
 while queue:
-    node = queue.popleft()
-    h, y, x = node
-    for direction in directions:
-        dh, dy, dx = direction
+    h, y, x = queue.popleft()
+    for dh, dy, dx in directions:
         nh, ny, nx = h + dh, y + dy, x + dx
-        if 0 <= nh < H and 0 <= ny < Y and 0 <= nx < X:
-            if not visited[nh][ny][nx]:
-                if graph[nh][ny][nx] == 0:
-                    queue.append((nh, ny, nx))
-                    visited[nh][ny][nx] = True
-                    graph[nh][ny][nx] = graph[h][y][x] + 1
+        if 0 <= nh < H and 0 <= ny < N and 0 <= nx < M and graph[nh][ny][nx] == 0:
+            graph[nh][ny][nx] = graph[h][y][x] + 1
+            queue.append((nh, ny, nx))
 
-for xy in graph:
-    for x in xy:
-        if 0 in x:
-            cannot_riped = True
-        if ans < max(x):
-            ans = max(x)
-
-if already_riped:
-    print(0)
-elif cannot_riped:
-    print(-1)
-else:
-    print(ans - 1)
+ans = 0
+for layer in graph:
+    for row in layer:
+        if 0 in row:
+            print(-1)
+            exit(0)
+        ans = max(ans, max(row))
+print(ans - 1)

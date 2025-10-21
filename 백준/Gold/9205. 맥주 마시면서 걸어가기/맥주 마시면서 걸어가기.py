3f32c948fc0792c3706792b0815bbce622322139
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2025-10-22 01:21:56 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 156 ms, Memory: 34984 KB -BaekjoonHub

---
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.py
@@ -0,0 +1,42 @@
+from collections import deque, defaultdict
+
+T = int(input())
+LIMIT = 1000
+
+
+def movable(p1, p2):
+    if 0 < (abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])) <= LIMIT:
+        return True
+    else:
+        return False
+
+
+for _ in range(T):
+    N = int(input())
+    home = tuple(map(int, input().split()))
+    stores = [tuple(map(int, input().split())) for _ in range(N)]
+    festival = tuple(map(int, input().split()))
+
+    stores.append(home)
+    stores.append(festival)
+
+    visited = []
+    graph = defaultdict(list)
+    for i in range(len(stores)):
+        for j in range(len(stores)):
+            if movable(stores[i], stores[j]):
+                graph[stores[i]].append(stores[j])
+
+    queue = deque([home])
+    visited.append(home)
+    while queue:
+        node = queue.popleft()
+        if node == festival:
+            print("happy")
+            break
+        for next in graph[node]:
+            if movable(node, next) and next not in visited:
+                queue.append(next)
+                visited.append(next)
+    else:
+        print("sad")

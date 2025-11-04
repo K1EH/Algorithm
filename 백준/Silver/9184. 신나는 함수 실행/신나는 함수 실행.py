5dcf52e55acb07048b3444e82756ab9efe223036
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2025-11-04 17:47:20 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 628 ms, Memory: 40560 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,37 +1,25 @@
-dp = [[[0] * 21 for _ in range(21)] for _ in range(21)]
-
 def w(a, b, c):
+    if dp[a + 50][b + 50][c + 50] != -1:
+        return dp[a + 50][b + 50][c + 50]
     if a <= 0 or b <= 0 or c <= 0:
-        return 1
+        dp[a + 50][b + 50][c + 50] =  1
     elif a > 20 or b > 20 or c > 20:
-        if dp[20][20][20]:
-            return dp[20][20][20]
-        else:
-            dp[20][20][20] = w(20, 20, 20)
-            return dp[20][20][20]
-    elif a < b and b < c:
-        if dp[a][b][c-1]: o = dp[a][b][c-1]
-        else: o = w(a, b, c-1)
-        if dp[a][b-1][c-1]: p = dp[a][b-1][c-1]
-        else: p = w(a, b, c-1)
-        if dp[a][b-1][c]: q = dp[a][b-1][c]
-        else: q = w(a, b-1, c) 
-        dp[a][b][c] = o + p - q
+        dp[a + 50][b + 50][c + 50] =  w(20, 20, 20)
+    elif a < b < c:
+        dp[a + 50][b + 50][c + 50] =  w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
     else:
-        if dp[a-1][b][c]: o = dp[a-1][b][c]
-        else: o = w(a-1, b, c)
-        if dp[a-1][b-1][c]: p = dp[a-1][b-1][c]
-        else: p = w(a-1, b-1, c)
-        if dp[a-1][b][c-1]: q = dp[a-1][b][c-1]
-        else: q = w(a-1, b, c-1)
-        if dp[a-1][b-1][c-1]: r = dp[a-1][b-1][c-1]
-        else: r = w(a-1, b-1, c-1)
-        dp[a][b][c] = o + p + q - r
-    return dp[a][b][c]
+        dp[a + 50][b + 50][c + 50] =  (
+            w(a - 1, b, c)
+            + w(a - 1, b - 1, c)
+            + w(a - 1, b, c - 1)
+            - w(a - 1, b - 1, c - 1)
+        )
+    return dp[a + 50][b + 50][c + 50]
 
+
+dp = list(list([-1] * 101 for _ in range(101)) for _ in range(101))
 while True:
     a, b, c = map(int, input().split())
-    if a == -1 and b == -1 and c == -1:
+    if a == b == c == -1:
         break
-    else:
-        print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
+    print(f"w({a}, {b}, {c}) = {w(a, b, c)}")

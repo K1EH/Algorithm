ba483a74c1658aa1bdeaa01128c45e3b948f6923
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-16 18:08:15 +0900

    [Platinum V] Title: 체커, Time: 156 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.py
+++ 백준/Platinum/1090. 체커/체커.py
@@ -1,21 +1,18 @@
 INF = 1e9
+
 N = int(input())
+checkers = [list(map(int, input().split())) for _ in range(N)]
+
 answer = [INF] * N
 answer[0] = 0
 
-checkers = [list(map(int, input().split())) for _ in range(N)]
-xs = [x for x, y in checkers]
-ys = [y for x, y in checkers]
-for x in xs:
-    for y in ys:
+for dx, _ in checkers:
+    for _, dy in checkers:
         move = []
-        for checker in checkers:
-            dx, dy = checker
+        for x, y in checkers:
             move.append(abs(x - dx) + abs(y - dy))
         move.sort()
-
-        for i in range(1, N):
-            candidate = sum(move[: i + 1])
-            if answer[i] > candidate:
-                answer[i] = candidate
+        for i in range(N):
+            if answer[i] > sum(move[: i + 1]):
+                answer[i] = sum(move[: i + 1])
 print(*answer)

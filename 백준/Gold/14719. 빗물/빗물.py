fe99372179ef1eb9883ed0b2a178cc631d123f2e
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-19 17:24:23 +0900

    [Gold V] Title: 빗물, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.py
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -1,11 +1,7 @@
+ans = 0
 H, W = map(int, input().split())
-cols = list(map(int, input().split()))
+blocks = list(map(int, input().split()))
 
-drops = 0
-
-for i in range(1, W - 1):
-    left_wall, right_wall = max(cols[0:i]), max(cols[i + 1 :])
-    if cols[i] < left_wall and cols[i] < right_wall:
-        drops += min(left_wall, right_wall) - cols[i]
-
-print(drops)
+for i in range(1, len(blocks) - 1):
+    ans += max(0, min(max(blocks[:i]), max(blocks[i + 1 :])) - blocks[i])
+print(ans)

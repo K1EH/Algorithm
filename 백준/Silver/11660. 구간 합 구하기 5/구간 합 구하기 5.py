8e4ffdac9222e1743c5f55869cf043118943d36b
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-19 17:46:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 3592 ms, Memory: 131772 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,21 +1,22 @@
 N, M = map(int, input().split())
-grid = [list(map(int, input().split())) for _ in range(N)]
-pos = [list(map(int, input().split())) for _ in range(M)]
+arr, coordinates = [], []
+for _ in range(N):
+    arr.append(list(map(int, input().split())))
+for _ in range(M):
+    coordinates.append(list(map(int, input().split())))
 
-prefix_sum = [[0] * (N + 1) for _ in range(N + 1)]
-for x in range(1, N + 1):
-    for y in range(1, N + 1):
-        prefix_sum[x][y] = (
-            grid[x - 1][y - 1]
-            + prefix_sum[x - 1][y]
-            + prefix_sum[x][y - 1]
-            - prefix_sum[x - 1][y - 1]
+prefix = [list(0 for _ in range(N + 1)) for _ in range(N + 1)]
+for y in range(N):
+    for x in range(N):
+        prefix[y + 1][x + 1] = (
+            arr[y][x] + prefix[y][x + 1] + prefix[y + 1][x] - prefix[y][x]
         )
-for p in pos:
-    x1, y1, x2, y2 = p
-    print(
-        prefix_sum[x2][y2]
-        - prefix_sum[x1 - 1][y2]
-        - prefix_sum[x2][y1 - 1]
-        + prefix_sum[x1 - 1][y1 - 1]
+
+for y1, x1, y2, x2 in coordinates:
+    ans = (
+        prefix[y2][x2]
+        - prefix[y1 - 1][x2]
+        - prefix[y2][x1 - 1]
+        + prefix[y1 - 1][x1 - 1]
     )
+    print(ans)

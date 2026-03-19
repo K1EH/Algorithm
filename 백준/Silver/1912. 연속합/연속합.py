1ace225648a5ed646520f44e3b2cb71d6c0e9f2d
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-19 16:09:58 +0900

    [Silver II] Title: 연속합, Time: 84 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,8 +1,9 @@
 INF = 1e9
-N = int(input())
-nums = list(map(int, input().split()))
-prefix_sum = [-INF] * (N + 1)
 
+N = int(input())
+arr = list(map(int, input().split()))
+prefix = [-INF for _ in range(N + 1)]
 for i in range(N):
-    prefix_sum[i + 1] = max(prefix_sum[i] + nums[i], nums[i])
-print(max(prefix_sum))
+    prefix[i + 1] = max(arr[i] + prefix[i], arr[i])
+
+print(max(prefix))

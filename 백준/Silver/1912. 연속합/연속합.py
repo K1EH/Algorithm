206252bb8064e972886f1018474342d3f376631d
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-20 17:57:19 +0900

    [Silver II] Title: 연속합, Time: 92 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -1,9 +1,8 @@
-INF = 1e9
-
 N = int(input())
 arr = list(map(int, input().split()))
-prefix = [-INF for _ in range(N + 1)]
-for i in range(N):
-    prefix[i + 1] = max(arr[i] + prefix[i], arr[i])
 
-print(max(prefix))
+prefix = [0 for _ in range(N + 1)]
+for i in range(N):
+    prefix[i + 1] = max(arr[i], prefix[i] + arr[i])
+
+print(max(prefix[1:]))

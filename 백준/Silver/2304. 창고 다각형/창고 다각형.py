9e844a47892c86c535139c382921ea730e48d57b
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-20 18:58:38 +0900

    [Silver II] Title: 창고 다각형, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -1,19 +1,22 @@
+def area(arr):
+    a = 0
+    location, height = arr[0]
+    for L, H in arr[1:]:
+        if H >= height:
+            a += abs(L - location) * height
+            location, height = L, H
+    return a
+
+
 N = int(input())
 ans = 0
-columns = []
-for _ in range(N):
-    columns.append(list(map(int, input().split())))
+columns = [list(map(int, input().split())) for _ in range(N)]
 columns.sort()
 
-
 idx = columns.index(max(columns, key=lambda x: x[1]))
-left_columns = columns[: idx + 1]
-right_columns = sorted(columns[idx:], reverse=True)
 
-for col in [left_columns, right_columns]:
-    pos, height = col[0]
-    for p, h in col[1:]:
-        if h >= height:
-            ans += abs(p - pos) * height
-            pos, height = p, h
-print(ans + columns[idx][1])
+left_columns = columns[: idx + 1]
+right_columns = columns[idx:][::-1]
+
+ans = area(left_columns) + area(right_columns) + columns[idx][1]
+print(ans)

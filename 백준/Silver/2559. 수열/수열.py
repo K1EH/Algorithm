6e3016362ec61c3f80b19baabe0239cc57a8e811
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-20 17:44:37 +0900

    [Silver III] Title: 수열, Time: 88 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,12 +1,14 @@
-N, interval = map(int, input().split())
+ans = -1e9
 
-days = list(map(int, input().split()))
+N, K = map(int, input().split())
+arr = list(map(int, input().split()))
 prefix = [0 for _ in range(N + 1)]
+
 for i in range(N):
-    prefix[i + 1] = prefix[i] + days[i]
+    prefix[i + 1] = prefix[i] + arr[i]
 
-interval_sum = []
-for i in range(interval, N + 1):
-    interval_sum.append(prefix[i] - prefix[i - interval])
-
-print(max(interval_sum))
+for i in range(K, N + 1):
+    temp = prefix[i] - prefix[i - K]
+    if ans < temp:
+        ans = temp
+print(ans)

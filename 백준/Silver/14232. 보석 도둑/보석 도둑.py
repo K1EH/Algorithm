7cd0c5323d967e4ad0b4e95f77db443376f94e95
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-21 21:03:05 +0900

    [Silver I] Title: 보석 도둑, Time: 144 ms, Memory: 43916 KB -BaekjoonHub

--- 백준/Silver/14232. 보석 도둑/보석 도둑.py
+++ 백준/Silver/14232. 보석 도둑/보석 도둑.py
@@ -1,22 +1,22 @@
-def sieve(N):
-    is_prime = [True] * (N + 1)
+def sieve(n):
+    is_prime = [True] * (n + 1)
     is_prime[0] = is_prime[1] = False
-    for i in range(2, int(N**0.5)):
-        if is_prime[i]:
-            for j in range(i * i, N + 1, i):
-                is_prime[j] = False
-    prime = [p for p, v in enumerate(is_prime) if v]
-    return prime
+    for prime in range(2, int(n**0.5) + 1):
+        if is_prime[prime]:
+            for multiple in range(prime * prime, n + 1, prime):
+                is_prime[multiple] = False
+    return [i for i, v in enumerate(is_prime) if v]
 
 
 ans = []
-N = int(input())
-prime = sieve(int(N**0.5) + 1)
-for p in prime:
-    while N % p == 0:
-        N //= p
-        ans.append(p)
-if N != 1:
-    ans.append(N)
+k = int(input())
+primes = sieve(int(k**0.5) + 1)
+for prime in primes:
+    while k % prime == 0:
+        ans.append(prime)
+        k //= prime
+
+if k != 1:
+    ans.append(k)
 print(len(ans))
 print(*ans)

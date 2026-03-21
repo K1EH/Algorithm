e533e0430acd8657b4c6cbe9925e6296299e6144
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-21 15:55:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 3496 ms, Memory: 131076 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,22 +1,18 @@
 N, M = map(int, input().split())
-arr, coordinates = [], []
-for _ in range(N):
-    arr.append(list(map(int, input().split())))
-for _ in range(M):
-    coordinates.append(list(map(int, input().split())))
+arr = [list(map(int, input().split())) for _ in range(N)]
+coordinates = [list(map(int, input().split())) for _ in range(M)]
+prefix = [[0] * (N + 1) for _ in range(N + 1)]
 
-prefix = [list(0 for _ in range(N + 1)) for _ in range(N + 1)]
 for y in range(N):
     for x in range(N):
         prefix[y + 1][x + 1] = (
             arr[y][x] + prefix[y][x + 1] + prefix[y + 1][x] - prefix[y][x]
         )
-
 for y1, x1, y2, x2 in coordinates:
     ans = (
         prefix[y2][x2]
-        - prefix[y1 - 1][x2]
         - prefix[y2][x1 - 1]
+        - prefix[y1 - 1][x2]
         + prefix[y1 - 1][x1 - 1]
     )
     print(ans)

a4392037c9757780aaa567adbb39c830602b0dbc
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-21 16:12:59 +0900

    [Gold V] Title: 개똥벌레, Time: 308 ms, Memory: 56608 KB -BaekjoonHub

--- 백준/Gold/3020. 개똥벌레/개똥벌레.py
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -2,22 +2,28 @@ import sys
 
 input = sys.stdin.readline
 
+obs, count = 1e9, 1
+
 N, H = map(int, input().split())
-columns = []
-for _ in range(N):
-    columns.append(int(input()))
-imos = list(0 for _ in range(H))
+
+arr = [0] * H
+prefix = [0] * (H + 1)
 
 for i in range(N):
+    length = int(input())
+    if i % 2 == 0:
+        arr[0] += 1
+        arr[length] -= 1
+    else:
+        arr[H - length] += 1
 
-    if i % 2 == 0:  # even
-        imos[0] += 1
-        imos[columns[i]] -= 1
-    else:  # odd
-        imos[H - columns[i]] += 1
-
-prefix = [0 for _ in range(H + 1)]
 for i in range(H):
-    prefix[i + 1] = prefix[i] + imos[i]
-ans = min(prefix[1:])
-print(ans, prefix[1:].count(ans))
+    p = prefix[i] + arr[i]
+    prefix[i + 1] = p
+    if p < obs:
+        obs = p
+        count = 1
+    elif p == obs:
+        count += 1
+
+print(obs, count)

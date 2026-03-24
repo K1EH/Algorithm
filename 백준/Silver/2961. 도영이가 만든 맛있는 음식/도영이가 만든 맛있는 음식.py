b5482336a1af2704b7ead1967c35aa9f0da604ba
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-03-24 18:00:01 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,21 +1,17 @@
-INF = 1e10
+answer = 1e9
 N = int(input())
 ingredient = [list(map(int, input().split())) for _ in range(N)]
 
 
-flavor = INF
-
-
-def rec(idx, sour, bitter, used):
-    global flavor
+def recursion(idx, S, B, used):
+    global answer
     if idx == N:
         if used:
-            flavor = min(flavor, abs(sour - bitter))
+            answer = min(answer, abs(S - B))
         return
-    rec(idx + 1, sour * ingredient[idx][0], bitter + ingredient[idx][1], True)
-    rec(idx + 1, sour, bitter, used)
+    recursion(idx + 1, S * ingredient[idx][0], B + ingredient[idx][1], used + 1)
+    recursion(idx + 1, S, B, used)
 
 
-rec(0, 1, 0, False)
-
-print(flavor)
+recursion(0, 1, 0, 0)
+print(answer)

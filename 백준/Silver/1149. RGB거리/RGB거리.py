0cd102019fa41cd2d187b0b59dc97ffa359de38b
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-04-01 17:50:52 +0900

    [Silver I] Title: RGB거리, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,13 +1,10 @@
 N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0, 0, 0] for _ in range(N)]
 
-house = [list(map(int, input().split())) for _ in range(N)]
-
-dp = [[0] * 3 for _ in range(N)]
-dp[0] = house[0]
+dp[0] = arr[0].copy()
 
 for i in range(1, N):
-    dp[i][0] = house[i][0] + min(dp[i - 1][1], dp[i - 1][2])
-    dp[i][1] = house[i][1] + min(dp[i - 1][0], dp[i - 1][2])
-    dp[i][2] = house[i][2] + min(dp[i - 1][0], dp[i - 1][1])
-
+    for color in range(3):
+        dp[i][color] = arr[i][color] + min(dp[i - 1][color - 1], dp[i - 1][color - 2])
 print(min(dp[N - 1]))

4953dbed9a8efdb9938c55765d0b80169d18bfe5
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-04-01 19:21:37 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,11 +1,14 @@
 N = int(input())
-stairs = [0] * (N + 1)
-for i in range(1, N + 1):
-    stairs[i] = int(input())
-dp = [0] * (N + 1)
-if N <= 2:
-    print(sum(stairs))
-else:
-    for i in range(1, N + 1):
+stairs = list(int(input()) for _ in range(N))
+dp = [0] * N
+
+for i in range(N):
+    if i == 0:
+        dp[i] = stairs[i]
+    elif i == 1:
+        dp[i] = stairs[i] + stairs[i - 1]
+    elif i == 2:
+        dp[i] = stairs[i] + max(stairs[i - 1], stairs[i - 2])
+    else:
         dp[i] = stairs[i] + max(stairs[i - 1] + dp[i - 3], dp[i - 2])
-    print(dp[N])
+print(dp[N - 1])

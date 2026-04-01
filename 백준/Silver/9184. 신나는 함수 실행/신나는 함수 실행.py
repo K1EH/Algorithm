6f8350ae77345d5851cc9c7c7dc8d50b4d75b9e0
CHOI JITAE <96415949+K1EH@users.noreply.github.com>
2026-04-01 17:50:02 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 608 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -1,25 +1,29 @@
+dp = [[[0 for _ in range(21)] for _ in range(21)] for _ in range(21)]
+
+
 def w(a, b, c):
-    if dp[a + 50][b + 50][c + 50] != -1:
-        return dp[a + 50][b + 50][c + 50]
     if a <= 0 or b <= 0 or c <= 0:
-        dp[a + 50][b + 50][c + 50] =  1
-    elif a > 20 or b > 20 or c > 20:
-        dp[a + 50][b + 50][c + 50] =  w(20, 20, 20)
-    elif a < b < c:
-        dp[a + 50][b + 50][c + 50] =  w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
+        return 1
+    if a > 20 or b > 20 or c > 20:
+        return w(20, 20, 20)
+    if dp[a][b][c]:
+        return dp[a][b][c]
+    elif a < b and b < c:
+        dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c)
     else:
-        dp[a + 50][b + 50][c + 50] =  (
+        dp[a][b][c] = (
             w(a - 1, b, c)
             + w(a - 1, b - 1, c)
             + w(a - 1, b, c - 1)
             - w(a - 1, b - 1, c - 1)
         )
-    return dp[a + 50][b + 50][c + 50]
+    return dp[a][b][c]
 
 
-dp = list(list([-1] * 101 for _ in range(101)) for _ in range(101))
 while True:
     a, b, c = map(int, input().split())
-    if a == b == c == -1:
+
+    if [a, b, c] == [-1, -1, -1]:
         break
-    print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
+    else:
+        print(f"w({a}, {b}, {c}) = {w(a, b, c)}")
